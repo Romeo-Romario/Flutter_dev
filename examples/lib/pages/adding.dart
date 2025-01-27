b352152e77deccab4c2bde6c5c1541b86a64344d
Roman Krit <romankrit2005@gmail.com>
2025-01-28 00:06:58 +0200

    Add 'Add element window' but it is not working so far

--- examples/lib/pages/adding.dart
+++ examples/lib/pages/adding.dart
@@ -8,16 +8,88 @@ class AddingEl extends StatefulWidget {
 }
 
 class _AddingElState extends State<AddingEl> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final mainTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[200],
-        title: Text("Add the element"),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Text('Adding screen'),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[200],
+          title: Text("Add the element"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 30, 40, 0),
+                child: TextFormField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter title of element',
+                      labelText: 'Title'),
+                  validator: (String? value) {
+                    if (titleController.text == null ||
+                        titleController.text.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 10, 40, 0),
+                child: TextFormField(
+                  controller: mainTextController,
+                  maxLines: 7,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Write some text (Optional)",
+                    labelText: "Main text",
+                    alignLabelWithHint: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color?>(
+                            Colors.purple[400])),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: Text("Wrong Input"),
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                      child: Text(
+                        "Add element",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+        ));
   }
 }
