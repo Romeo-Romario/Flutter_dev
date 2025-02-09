a4d2a6eeb9a54823dff0236cd03a3e4532c50698
Roman Krit <romankrit2005@gmail.com>
2025-02-09 15:28:05 +0200

    Each element can be opened

---
+++ examples/lib/services/widgets/element_display.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'dart:io';
+
+import 'package:examples/services/models/image_source_type.dart';
+import 'package:flutter/material.dart';
+import 'package:examples/services/models/book_info.dart';
+
+class ElementDisplay extends StatelessWidget {
+  const ElementDisplay({super.key, required this.bookInfo});
+
+  final BookInfo bookInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Element"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: switch (bookInfo.imageSource) {
+                  ImageSourceType.asset => Image.asset(bookInfo.imagePath),
+                  ImageSourceType.web => Image.network(bookInfo.imagePath),
+                  ImageSourceType.local =>
+                    Image(image: FileImage(File(bookInfo.imagePath))),
+                },
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Center(
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 5.0),
+                    child: Text(
+                      bookInfo.title,
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(color: Colors.black, width: 2.0)),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    bookInfo.text,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(color: Colors.black, width: 2.0)),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- examples/lib/services/widgets/element_widget.dart
+++ examples/lib/services/widgets/element_widget.dart
@@ -1,4 +1,5 @@
 import 'package:examples/services/models/image_source_type.dart';
+import 'package:examples/services/widgets/element_display.dart';
 import 'package:flutter/material.dart';
 import 'package:examples/services/models/book_info.dart';
 import 'dart:io';
@@ -10,17 +11,26 @@ class BookInfoView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: ListTile(
-        title: Text(element.title),
-        subtitle: Text(element.text),
-        leading: switch (element.imageSource) {
-          ImageSourceType.asset => Image.asset(element.imagePath),
-          ImageSourceType.web => Image.network(element.imagePath),
-          ImageSourceType.local =>
-            Image(image: FileImage(File(element.imagePath))),
-        },
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ElementDisplay(bookInfo: element)),
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: ListTile(
+          title: Text(element.title),
+          subtitle: Text(element.text),
+          leading: switch (element.imageSource) {
+            ImageSourceType.asset => Image.asset(element.imagePath),
+            ImageSourceType.web => Image.network(element.imagePath),
+            ImageSourceType.local =>
+              Image(image: FileImage(File(element.imagePath))),
+          },
+        ),
       ),
     );
   }
